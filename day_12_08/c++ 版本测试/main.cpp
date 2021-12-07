59ea6592ac1181f243a196d80c2da28b8cf13770
jeffygao <931312254@qq.com>
2021-12-08 00:16:27 +0800

    C++版本测试

---
+++ day_12_08/c++ 版本测试/main.cpp
@@ -0,0 +1,15 @@
+/*
+    这个代码可以测试 c++ 的版本
+
+    输出        代表
+    199711L     C98版本
+    201103L     C11版本
+
+    我的： 201402
+*/
+
+#include <iostream>
+
+int main( ){
+    std::cout << __cplusplus;
+}
