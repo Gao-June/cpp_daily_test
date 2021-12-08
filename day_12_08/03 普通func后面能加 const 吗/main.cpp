0344a23b42ea5dde89162125a8f2ad967f8263fd
jeffygao <931312254@qq.com>
2021-12-08 16:22:09 +0800

    普通 func后加 const可行不？

---
+++ day_12_08/03 普通func后面能加 const 吗/main.cpp
@@ -0,0 +1,22 @@
+/*
+    普通函数后面能加 const 吗？
+    不能！ 只有 成员函数 才可以
+    【注】成员函数后加 const 的目的是为了 禁止对class里成员变量的更改。
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int sum( int a, int b ) const {
+    return a + b;
+}
+
+int main( ){
+    int a = 10, b = 20;
+
+    // 报错： on-member function 'int sum(int, int)' cannot have cv-qualifier
+    cout << sum( a, b ) << endl;
+
+    return 0;
+}
