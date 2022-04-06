61d6c1d057e519d92fa79d0426e0957a7141fa36
note-jeffy <931312254@qq.com>
2022-04-06 23:36:53 +0800

    float

---
+++ mon_4/day_04_06/03_float_double/main.cpp
@@ -0,0 +1,13 @@
+// 在创建 float double的时候，会显示都是 double变量
+// 当想创建 float时应该在其后加上 f、F
+
+#include <iostream>
+
+int main( ){
+    float var1 = 0.55;  // 鼠标放在数字上，显示的是 double
+    double var2 = 0.53;
+    float var3 = 0.54f; // float
+
+
+    return 0;
+}
