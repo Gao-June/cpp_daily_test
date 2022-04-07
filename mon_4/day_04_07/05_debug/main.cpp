816f51d4269cca130ae83b40fae8e972cdcd678e
lib_jeffygao <gaojune_better@qq.com>
2022-04-07 16:46:21 +0800

    1

---
+++ mon_4/day_04_07/05_debug/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main( ){
+    int a = 9;
+    a ++;
+
+    std::string s = "hello";
+    for( int i = 1; i <= 5; i ++ ){
+        std::cout << s << std::endl;
+    }
+
+    return 0;
+}
