bb41bef2f44c768a3eb412e65463ed2338d58ab2
lib_jeffygao <gaojune_better@qq.com>
2022-04-14 14:05:59 +0800

    enum

--- mon_4/day_04_14/03_enum/main.cpp
+++ mon_4/day_04_14/03_enum/main.cpp
@@ -2,14 +2,22 @@
 
 #include <iostream>
 
+// 这里的 numbers 就相当于一个类型了
 enum numbers : int
 {
     A = 0, B, C = 5, D
 };
 
+void print_fun( numbers a ){
+    std::cout << "this number is : " << a << std::endl;;
+}
+
 int main( ){
 
     std::cout << "A: " << A << "  B: " << B << "  C: " << C  << " D: " << D << std::endl;
+
+    numbers num = D;
+    print_fun(D);
 
     return 0;
 }
