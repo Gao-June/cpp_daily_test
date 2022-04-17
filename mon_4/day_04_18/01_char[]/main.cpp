be554771046424365e9f32b50c26db1e0ef12d65
note-jeffy <931312254@qq.com>
2022-04-18 00:25:58 +0800

    1

---
+++ mon_4/day_04_18/01_char[]/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main( ){
+    char name[6] = {'a', 'b', 'c', 'd', 'e', 'f'}; 
+
+    std::cout << name << std::endl;
+
+    return 0;
+}
---
+++ mon_4/day_04_18/02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main( ){
+    char name1[6] = {'a', 'b', 'c', 'd', 'e', '\0'};    
+    std::cout << name1 << std::endl;     //abcdef
+
+    char name2[6] = {'a', 'b', 'c', 'd', 'e', 'f'};    
+
+    std::cout << name2 << std::endl;     //abcdef?v »áÂÒ
+
+
+
+
+    return 0;
+}
