202a138cb0a7669fccae1908d6d34e4003025335
note-jeffy <931312254@qq.com>
2022-04-20 11:18:17 +0800

    手写 string

--- mon_4/day_04_20/01_手写string/main.cpp
+++ mon_4/day_04_20/01_手写string/main.cpp
@@ -11,27 +11,31 @@ public:
     // 默认构造函数, 浅拷贝
     String( const char* str )
     {
+        std::cout << "调用了默认构造函数" << std::endl;
         _Size_Buffer = strlen( str );
         _Buffer = new char[_Size_Buffer + 1];
         memcpy( _Buffer, str, _Size_Buffer + 1 );
     }
 
-    // 拷贝构造函数，深拷贝
+    //拷贝构造函数，深拷贝
     String( const String& other_str ) : _Size_Buffer( other_str._Size_Buffer )
     {
+        std::cout << "调用了 深拷贝" << std::endl;
         _Buffer = new char[_Size_Buffer + 1];
         memcpy( _Buffer, other_str._Buffer, _Size_Buffer + 1 );
     }
 
     // operator <<
-    friend std::ostream& operator<< ( std::ostream &stream, const String string  );
+    // 【注】 后面如果写成 const String string 的话，还会调用一次深拷贝哦~
+    // 所以传参的时候一般都是  const + & 结合
+    friend std::ostream& operator<< ( std::ostream &stream, const String& string  );
 
     // operator[]
     char& operator[] ( unsigned int index );
 
 };
 
-std::ostream& operator<< ( std::ostream &stream, const String string )
+std::ostream& operator<< ( std::ostream &stream, const String& string )
 {
     stream << "buffer: " <<  string._Size_Buffer << "  string:  " << string._Buffer << std::endl;
     return stream;
@@ -46,25 +50,15 @@ char& String::operator[] ( unsigned int index )
 
 int main()
 {
+// 把深拷贝代码注释掉，就会调用默认的拷贝构造函数(浅拷贝)
     String str1( "hello_01" );
-    std::cout << str1 << std::endl;
-
-// 把深拷贝代码注释掉，就会调用默认的拷贝构造函数
-    std::cout << "使用浅拷贝" << std::endl;
-    String str2(str1);      
+    String str2(str1);
+    std::cout << "using operator<< :" << std::endl;       
     std::cout << str2 << std::endl;
     str2[2] = 'E';
     std::cout << str1 << std::endl;
     std::cout << str2 << std::endl;
 
-// 使用深拷贝
-    std::cout << "使用深拷贝" << std::endl;
-    String str3( "yes_sir" );
-    String str4(str3);      
-    std::cout << str4 << std::endl;
-    str4[2] = 'E';
-    std::cout << str3 << std::endl;
-    std::cout << str4 << std::endl;
 
     return 0;
 }
