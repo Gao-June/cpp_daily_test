1b1b95923ce3457999b5bc6a14cde88214c3125f
note-jeffy <931312254@qq.com>
2022-04-21 15:05:33 +0800

    函数指针

--- mon_4/day_04_21/02_函数指针/main.cpp
+++ mon_4/day_04_21/02_函数指针/main.cpp
@@ -1,5 +1,6 @@
 /**
  * 函数指针功能：让代码更干净
+ * 可以直接使用 auto = 函数名  的方式进行函数指针赋值
 */
 
 #include <iostream>
@@ -19,12 +20,20 @@ int main()
 {
     const int num1 = 10, num2 = 25;
 
+// 方式 - 1
     int (*p)( const int &, const int & );
     p = Add_nums;
     std::cout << p(num1, num2) << std::endl;
 
     p = Sub_num;
     std::cout << p(num1, num2) << std::endl;
+
+// 方式 - 2
+    auto q = Add_nums;
+    std::cout << q(num1, num2) << std::endl;
+
+    q = Sub_num;
+    std::cout << q(num1, num2) << std::endl;
 
     return 0;
 }
