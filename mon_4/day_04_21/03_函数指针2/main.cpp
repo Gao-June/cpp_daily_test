3861b44ac7082faef91f0cbe0e58ba764d27e862
note-jeffy <931312254@qq.com>
2022-04-21 15:32:38 +0800

    函数指针

---
+++ mon_4/day_04_21/03_函数指针2/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * 与上一个（函数指针1）作为区分，这里写一个实例
+ * 测试 函数指针 作为函数的参数进行调用的情况。
+ *  在 函数 Func_each() 中使用 Print() 这个函数
+*/
+
+#include <iostream>
+#include <vector>
+
+// 打印元素
+void Print( int Num )
+{
+    std::cout << "num value is : " << Num << std::endl;
+}
+
+// 遍历 vector 中的元素， 第2个参数是函数指针
+void Fun_each( std::vector<int> Nums, void(*Ptr)(int) )
+{
+    for (auto &num : Nums )
+    {
+        Ptr( num );
+    }
+}
+
+int main()
+{
+    std::vector<int> Nums{1,3,5,7,3};
+    Fun_each(Nums, Print);
+
+    return 0;
+}
