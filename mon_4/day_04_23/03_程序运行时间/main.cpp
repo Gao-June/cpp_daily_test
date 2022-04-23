3de42b44e0aff671c806a33c641deb79d0b9acbf
note-jeffy <931312254@qq.com>
2022-04-23 22:06:44 +0800

    检验程序运行时间

---
+++ mon_4/day_04_23/03_程序运行时间/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * 检验程序运行时间 
+*/
+
+#include <iostream>
+#include <thread>
+#include <chrono>       // 时钟
+
+int main( ){
+
+    auto Start = std::chrono::high_resolution_clock::now();
+
+    // 休眠 3S
+    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+    // for 循环执行 10E 次
+    // for( int i = 0; i < 1000000000; i ++ ){
+    //     ;
+    // }
+
+    auto End = std::chrono::high_resolution_clock::now();
+
+    // 求时间差， 这里也可以用 auto 的
+    std::chrono::duration<double> Duration = End - Start;
+    std::cout << Duration.count() << " s " << std::endl;
+
+    return 0;
+}
