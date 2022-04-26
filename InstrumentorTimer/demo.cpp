5df3eb1e1890e9ed7545c2d88c88bb91f8a0dddf
note-jeffy <931312254@qq.com>
2022-04-26 12:52:04 +0800

    测试-项目 Instrumentor Timer

---
+++ InstrumentorTimer/demo.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include "InstrumentorTimer.h"
+#include "InstrumentorMacro.h"
+
+namespace Benchmark {
+    int Fibonacci(int x) {
+        std::string name = std::string("Fibonacci ") + std::to_string(x);
+        PROFILE_SCOPE(name.c_str());
+
+        std::this_thread::sleep_for(std::chrono::microseconds(1)); // Compensate statistical temporal precision problem
+
+        if (x < 3) return 1;
+        int part1 = Fibonacci(x - 1);
+        int part2 = Fibonacci(x - 2);
+        return part1 + part2;
+    }
+
+    void RunBenchmarks() {
+        PROFILE_FUNCTION();
+
+        std::cout << "Running Benchmarks..." << std::endl;
+        std::thread t1([]() { Fibonacci(9); });
+        std::thread t2([]() { Fibonacci(11); });
+
+        t1.join();
+        t2.join();
+    }
+}
+
+int main() {
+    Instrumentor::BeginSession("Benchmark");
+    Benchmark::RunBenchmarks();
+    return 0;
+}
