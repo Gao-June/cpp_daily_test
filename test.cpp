f0644acad1a25e1a13194de0db192012ba00af47
note-jeffy <931312254@qq.com>
2022-06-09 17:13:29 +0800

    1

--- test.cpp
+++ test.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 
+class A{
+
+};
+
 class B{
 public:
     char ch = 'a';
@@ -12,33 +16,71 @@ class C{
 public:
     char ch1 = 'b';
     char ch2 = 'c';
-    virtual void func0(){}
+    virtual void func(){}
+    virtual void func1(){}
 };
 
-// 加上F作为测试
-class F{
+class D : public A, public C{
 public:
-    virtual void func3( ){}
+    int d = 9;
+    //virtual void func(){}
+    virtual void func2(){}
 };
 
 class E : public B, public C{
 public:
     int e = 1;
-    virtual void func0( ){}
+    virtual void func( ){}
     virtual void func1( ){}
-    //virtual void func3( ){}
+    // virtual void func3( ){}
+    // virtual void func4( ){}
+};
+
+
+// 加上F作为测试
+class F{
+public:
+    char ch4 = 'f';
+    int ff = 4;
+    virtual void func3( ){}
+};
+
+class G{
+public:
+    virtual void func4( ){}
+};
+
+class H{
+public:
+    virtual void func5( ){} 
+};
+
+class I : public G, public H{
+public:
+    virtual void func4( ){}
+    virtual void func5( ){} 
 };
 
 int main( ){
+    A a;
     B b;
     C c;
+    D d;
     E e;
     F f;
+    G g;
+    H h;
+    I i;
 
+    cout << "a: " << sizeof(a) << endl;     // 1
     cout << "b: " << sizeof(b) << endl;     // 16
-    cout << "c: " << sizeof(C) << endl;     // 16
-    cout << "f: " << sizeof(F) << endl;     // 8
-    cout << "e: " << sizeof(E) << endl;     // 32
-    cout << "查看地址：" << endl << "ch:" << &(e.ch) << "\tch1:" << &(e.ch1) << "\tch2:" <<
-        &(e.ch2) << "\te:" << &e.e << endl;
+    cout << "c: " << sizeof(c) << endl;     // 16
+    cout << "d: " << sizeof(d) << endl;     // 16
+    cout << "e: " << sizeof(e) << endl;     // 32
+    cout << "f: " << sizeof(f) << endl;     // 16
+    cout << "g: " << sizeof(g) << endl;     // 8
+    cout << "h: " << sizeof(h) << endl;     // 8
+    cout << "i: " << sizeof(i) << endl;     // 16
+    
+    return 0;
 }
